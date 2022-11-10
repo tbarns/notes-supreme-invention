7c8f172c16567f474eeb6341fd59a63b4fd25c80
Timothy Barnaby <tbarnaby1@gmail.com>
2022-11-10 15:44:25 -0600

    created path for DRUD operations

--- server.js
+++ server.js
@@ -4,64 +4,62 @@ const uuid = require('./helpers/uuid');
 const fs = require('fs');
 const util = require('util');
 const readFromFile = util.promisify(fs.readFile);
-
-const { dirname } = require('path');
-const { json } = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const db = require('./db/db.json');
+
 // const api = require("public/assets/js/index.js")
 
+// middleware
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname,'public')));
-// app.use('/api', api);
+app.use(express.static('public'));
 
-app.get('api/notes', (req, res)=>{
-  res.sendFile(path.join)(__dirname, './public/notes.html')
-});
 
 app.get('/api/notes', (req, res) => {
+  const savedNotes = db
+  fs.readFileSync(path.join(__dirname, './db/db.json'), 'UTF8', JSON.stringify(savedNotes))
+  res.json(savedNotes)
+  
+})
+
+app.post('/api/notes', (req, res) => {
+  const savedNotes = db
+  const newNote = { title: req.body.title, text: req.body.text, id: uuid() }
+  savedNotes.push(newNote)
+  console.log(savedNotes)
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(savedNotes))
+  res.json(savedNotes)
+})
+
+
+//selects a note from the param of the UUID that has
+app.delete('/api/notes/:id', (req, res) => {
+  const savedNotes = db
+  const dbNotes = savedNotes.filter(note => note.id !== req.params.id)
+  console.log(dbNotes)
+  
+
+ fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(dbNotes))
+ res.json(dbNotes)
+})
+
+
+//access the public folder to read the notes HTML
+app.get('/notes', (req, res) => {
   // Send a message to the client
-  res.status(200).json(`${req.method} successful  request received to get notes`);
-  res.status(404).json(`${req.method} failed request received to get notes`);
-  // Log our request to the terminal
-  console.info(`${req.method} request received to get notes`);
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+
+});
+app.get('*', (req, res) => {
+  // Send a message to the client
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+
 });
 
-
-app.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-});
-
-//GET Route for a specific note
-app.get('/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json', json)
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      const result = json.filter((note) => note.note_id === noteId);
-      return result.length > 0
-        ? res.json(result)
-        : res.json('No note with that ID');
-    });
-});
-
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
-
-
-// app.get("*",(req, res)=>{
-//   res.sendFile(path.join(__dirname, "/public/notes.html"))
-// })
-
-// app.get('/api/notes', (req,res) => {
-//   res.json(JSON.parse(data))
-// })
-
-
+// listening for requests
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
